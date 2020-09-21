d4023316632f90ac75399754df3d42eb84375845
zwp <zhangwanping@tpson.cn>
2020-09-21 14:52:24 +0800

    Add: 登录页面以及weight使用

---
+++ linearlayout/src/main/java/com/example/linearlayout/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn_login);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
